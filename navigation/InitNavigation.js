7e3d080b4a80589866fb17cfb411ba06b774b05c
Teddy Walter <teddywalter2016@gmail.com>
2021-02-26 17:08:26 +0000

    navigation mis en place

---
+++ navigation/InitNavigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import MainNavigation from './MainNavigation';
+
+const InitNavigation = () => {
+  return <MainNavigation />;
+};
+
+export default InitNavigation;
---
+++ navigation/MainNavigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+const MainNavigation = () => {
+  return <Text>MAIN NAVIGATION</Text>;
+};
+
+export default MainNavigation;
