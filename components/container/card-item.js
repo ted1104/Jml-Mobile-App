ff637f4da7d5556de9a782d83063429b5b3e2b29
Teddy Walter <teddywalter2016@gmail.com>
2021-02-26 21:34:37 +0000

    Fix Bug affichage icon

---
+++ components/container/card-item.js
@@ -0,0 +1,9 @@
+import {DefaultTheme} from '@react-navigation/native';
+import React from 'react';
+import {Text} from 'react-native';
+
+const CardItem = () => {
+  return <Text>CARD ITEM</Text>;
+};
+
+export default CardItem;
