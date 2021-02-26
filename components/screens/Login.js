b55d5859ef3e6ddb0a9973eac7b8af62124af893
Teddy Walter <teddywalter2016@gmail.com>
2021-02-26 18:16:24 +0000

    Login container

--- components/screens/Login.js
+++ components/screens/Login.js
@@ -1,10 +1,55 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+
+import Input from './../reusables/input';
+import Buttom from './../reusables/button';
 
 class LoginScreen extends React.Component {
   render() {
-    return <Text>LOGIN SCREEN</Text>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.containerTilte}>
+          <Text style={styles.title}>JML SYSTEM</Text>
+          <Text style={styles.titleSecond}>Se connecter !</Text>
+        </View>
+        <View style={styles.containerBloc}>
+          <Input placeholder="Nom d'utilisateur" />
+          <Input placeholder="Mot de passe" secureTextEntry={true} />
+          <Buttom />
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  containerTilte: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    // marginBottom: 20,
+  },
+  containerBloc: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 2,
+    color: '#0096c7',
+  },
+  titleSecond: {
+    fontSize: 20,
+    color: '#0096c7',
+    // marginBottom: 2,
+  },
+});
 
 export default LoginScreen;
